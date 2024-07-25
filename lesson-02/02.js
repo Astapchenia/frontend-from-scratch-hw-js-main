5df1dda58fa4363a8e412452e8789001ae0cc67e
Dzmitry Astapchenia <astapchenia@icloud.com>
2024-07-25 21:37:31 +0300

    corect task2-2-

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,16 +7,15 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 = Math.floor(Math.random() * 6) + 1; // your code
-let dice2 = Math.floor(Math.random() * 6) + 1; // your code
-let isWinningDouble = (dice1 === dice2) && (dice1 > 3) && (dice2 > 3); // your code
+let dice1 = Math.floor(Math.random() * 6) + 1; // генерируем случайное число от 1 до 6 для первого дайса
+let dice2 = Math.floor(Math.random() * 6) + 1; // генерируем случайное число от 1 до 6 для второго дайса
+let isWinningDouble = (dice1 === dice2 && dice1 > 3); // проверяем, является ли выпавший дубль выигрышным
 
-console.log('Первый бросок: ' + dice1)
-console.log('Второй бросок: ' + dice2)
+console.log('Первый бросок: ' + dice1);
+console.log('Второй бросок: ' + dice2);
 if (isWinningDouble) {
-  console.log('Выигрышный дубль!')
+  console.log('Выигрышный дубль!');
 } else {
-  console.log('Не выигрышный дубль.')
+  console.log('Не выигрышный дубль.');
 }
-
 
