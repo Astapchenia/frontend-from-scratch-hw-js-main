b775cb036fd2d9f39732deb92ea709f92b8c16ac
Dzmitry Astapchenia <astapchenia@icloud.com>
2024-07-29 19:49:44 +0300

    corect task3-1

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,7 +1,7 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
 function calculateAverage(a, b) {
-    const average = (a +b) /2;
+    const average = (a + b) / 2;
     return average;
 }
 
