5d4024942a262729a42664f5dcbd7ea864fcc2b9
Dzmitry Astapchenia <astapchenia@icloud.com>
2024-09-17 22:58:35 +0300

    task 9-1

--- lesson-09/01.js
+++ lesson-09/01.js
@@ -24,12 +24,12 @@ console.log(oddNumbers) // Должен вывести: [1, 3, 5]
 */
 
 const filter = (array, callback) => {
-  const newArr = [];
+  const newArray = []
   for (let index = 0; index < array.length; index++) {
-    const elemenet = array[index];
-    if (callback(element, index)) {
-      newArr.push(element)
+      const element = array[index]
+      if(callback(element, index)) {
+        newArray.push(element)
+      }
     }
+    return newArray
   }
-  return newArr;
-}
